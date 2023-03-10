c9ff80e63903bb25f00d568234c97ec9cacc1a26
“Abytrone” <abdulbasit.yahaya51@gmail.com>
2023-03-10 22:10:02 +0000

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-def print_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d} argument.".format(n))
-        return
-    else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-        i = 1
-        while i <= n:
-            print("{:d}: {:s}".format(i, argv[i]))
-            i += 1
+import sys
 
-if __name__ == "__main__":
-    import sys
-    print_arg(sys.argv)
+args = sys.argv[1:]
+num_args = len(args)
+
+# Print the number of arguments
+print("Number of argument" + ("s" if num_args > 1 else "") + ": " + str(num_args) + ("." if num_args == 0 else ":"))
+
+# Print the list of arguments
+if num_args > 0:
+    for i in range(num_args):
+        print(str(i+1) + ": " + args[i])
+        
